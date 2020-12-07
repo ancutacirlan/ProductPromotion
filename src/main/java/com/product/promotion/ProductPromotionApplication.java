f972a4b5a94268b46d6eb6d89d539dffaa29646a
Cirlan Ancuta <cirlan.ancutaa@gmail.com>
2020-12-07 19:53:52 +0200

    init app

---
+++ src/main/java/com/product/promotion/ProductPromotionApplication.java
@@ -0,0 +1,13 @@
+package com.product.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPromotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/promotion/ProductPromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
