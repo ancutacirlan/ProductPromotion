f75e520c104c7548b3233b0639385a9870433be8
Cirlan Ancuta <cirlan.ancutaa@gmail.com>
2020-12-07 22:13:23 +0200

    add dependinces, create sql tables

---
+++ src/main/resources/db/migration/V1.1__init.sql
@@ -0,0 +1,137 @@
+CREATE TABLE locations
+(
+    id                  serial,
+    city                varchar NOT NULL,
+    district            varchar NOT NULL,
+    country             varchar NOT NULL,
+    village             varchar,
+    street              varchar NOT NULL,
+    bloc                varchar,
+    stair               varchar,
+    no                  varchar NOT NULL,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key (id)
+);
+
+CREATE TABLE vegetables
+(
+    id                  serial,
+    name                varchar not null,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    PRIMARY KEY (id),
+    UNIQUE (name)
+
+);
+
+CREATE TABLE clients
+(
+    id                  serial,
+    first_name          varchar NOT NULL,
+    last_name           varchar NOT NULL,
+    phone               varchar NOT NULL,
+    email               varchar NOT NULL,
+    password            varchar,
+    location_id         int     NOT NULL,
+    valid_account       boolean NOT NULL            DEFAULT FALSE,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key (id),
+    unique (email,phone),
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+
+);
+
+CREATE TABLE producers
+(
+    id                  serial,
+    client_id           int     NOT NULL,
+    document            varchar NOT NULL,
+    active              boolean NOT NULL            DEFAULT FALSE,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key (id)
+);
+
+CREATE TABLE notices
+(
+    id                  serial,
+    producer_id         int NOT NULL,
+    vegetable_id        int NOT NULL,
+    quantity_available  int NOT NULL,
+    unit                varchar NOT NULL,
+    image               varchar,
+    home_delivery       boolean NOT NULL            DEFAULT FALSE,
+    product_notice      varchar,
+    valid               boolean NOT NULL            DEFAULT FALSE,
+    alive               boolean NOT NULL            DEFAULT FALSE,
+    price_per_unit      INTEGER NOT NULL,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key(id),
+    FOREIGN KEY (producer_id) REFERENCES producers (id),
+    FOREIGN KEY (vegetable_id) REFERENCES vegetables (id)
+);
+
+CREATE TABLE feedbacks
+(
+    client_id           int NOT NULL,
+    notices_id          int NOT NULL,
+    message             varchar,
+    qualifying          int NOT NULL,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key (client_id,notices_id),
+    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (notices_id) REFERENCES notices (id)
+);
+
+CREATE TABLE orders
+(
+    id                  serial,
+    client_id           int NOT NULL,
+    data                DATE NOT NULL,
+    confirmed           boolean NOT NULL           DEFAULT FALSE,
+    notice_order        varchar,
+    total_price         int  NOT NULL,
+    location_id         int,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+
+CREATE TABLE products
+(
+    id                  serial,
+    notice_id           int NOT NULL,
+    quantity            int NOT NULL,
+    mode_delivery       varchar NOT NULL,
+    order_id            int NOT NULL,
+    date_created        timestamp without time zone DEFAULT now(),
+    date_updated        timestamp without time zone DEFAULT now(),
+    is_deleted          boolean NOT NULL            DEFAULT FALSE,
+    primary key (id),
+    FOREIGN KEY (order_id) REFERENCES orders(id)
+);
+
+
+
+
+
+
+
+
+
+
+
